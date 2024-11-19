e9d26a0441a65027bda8221be9e81b31eb224479
rCodFish <elcfsantos@gmail.com>
2024-11-19 17:10:38 +0000

    layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,15 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="alerts" />
+      <Stack.Screen name="dataActivities" />
+      <Stack.Screen name="dataSteps" />
+      <Stack.Screen name="dataWater" />
+      <Stack.Screen name="homePage" />
       <Stack.Screen name="index" />
+      <Stack.Screen name="profile" />
+      <Stack.Screen name="signUp" />
+      <Stack.Screen name="signIn" />
     </Stack>
   );
 }
